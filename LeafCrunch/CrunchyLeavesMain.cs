d6879e0858c7ba600c50828e7fe1f037aceece0f
Eden Gravis <j.grav00@gmail.com>
2023-11-18 00:23:28 -0600

    beginnings of menu behavior

--- LeafCrunch/CrunchyLeavesMain.cs
+++ LeafCrunch/CrunchyLeavesMain.cs
@@ -1,7 +1,10 @@
 ﻿using LeafCrunch.GameObjects;
+using LeafCrunch.Menus;
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Linq;
+using LeafCrunch.Utilities;
 
 namespace LeafCrunch
 {
@@ -14,6 +17,8 @@ namespace LeafCrunch
     public partial class CrunchyLeavesMain : Form
     {
         private List<GenericGameObject> Objects { get; set; }
+        private List<GenericInterrupt> Interrupts { get; set; }
+
         public CrunchyLeavesMain()
         {
             InitializeComponent();
@@ -73,11 +78,34 @@ namespace LeafCrunch
                 stats,
                 Room
             };
+
+            Interrupts = new List<GenericInterrupt>()
+            {
+                new HelpMenu()
+                {
+                    IsActive = false,
+                    //Control = ???
+                }
+            };
+
             timer1.Start();
         }
 
         private void CrunchyLeavesMain_KeyDown(object sender, KeyEventArgs e)
         {
+            var relevantInterrupts = Interrupts.Where(i => i != null && !i.IsActive /*we don't need already active ones*/
+                                                            && i.ActivationKey == e.KeyCode);
+            foreach (var i in relevantInterrupts)
+            {
+                i.IsActive = true;
+            }
+
+            //don't waste time looping through objects if we're in a menu or paused
+            //note: we'll have to do more here later on as we flesh out the menus of course
+            //in case menus have key shortcuts
+            //hm i wonder if escape should close them actually
+            if (relevantInterrupts.Any()) return;
+            
             //go through list of objects and fire off keypress events
             //you know I also wonder if we want to just store the key in general and not just for the player
             foreach (var obj in Objects)
@@ -88,14 +116,32 @@ namespace LeafCrunch
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            var activeInterrupts = Interrupts.Where(i => i != null && i.IsActive);
+            foreach (var i in activeInterrupts)
+            {
+                var menu = i as GenericMenu;
+                if (menu != null) menu.Update();
+            }
+
+            if (!activeInterrupts.Any()) return; //don't do gameplay objects during interrupt.
             foreach (var obj in Objects)
             {
                 obj.Update();
             }
         }
 
+        //i wonder if i should have "random mode" and just it makes random leave controls everywhere
+
         private void CrunchyLeavesMain_KeyUp(object sender, KeyEventArgs e)
         {
+            if (Interrupts.Where(i => i != null && i.IsActive).Any()) return; //short circuit if a menu is up
+            //note: could cause an issue later though if we have a keydown and the person is still holding it when we do the menu
+            //maybe we should force a key up if that happens....somehow.
+            
+            //or we could store the active keys actually and then check if they're still pressed when we leave the menu.
+            //yeah that could work
+            //and then if not fire off the key up handler for the ones that aren't down anymore.
+
             //go through list of objects and fire off key up events
             foreach (var obj in Objects)
             {
---
+++ LeafCrunch/Menus/HelpMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeafCrunch.Menus
+{
+    //an object on the menu with its own update things
+    public class MenuItem
+    {
+        public void Update() { }
+    }
+
+    //mostly for menus but could be for pausing I guess
+    public abstract class GenericInterrupt
+    {
+        public bool IsActive { get; set; }
+        public Keys ActivationKey { get; set; }
+        public Control Control { get; set; } //parent control for menu items or message popup...like you show and hide this
+    }
+
+    public class GenericMenu : GenericInterrupt
+    {
+        public List<MenuItem> Items { get; set; }
+        virtual public void Update() { } //to fill in...probably loop through items and call their update functions
+    }
+
+    public class HelpMenu : GenericMenu
+    {
+        public HelpMenu() : base()
+        {
+            ActivationKey = Keys.H;
+        }
+
+    }
+}
