dcfa91bb8d18dea6b243ba648b694749e5319f55
Eden Gravis <j.grav00@gmail.com>
2023-11-24 10:59:41 -0600

    make layered sounds work

--- LeafCrunch/GameObjects/Items/InstantItems/Leaf.cs
+++ LeafCrunch/GameObjects/Items/InstantItems/Leaf.cs
@@ -2,8 +2,10 @@
 using LeafCrunch.GameObjects.Items.ItemOperations;
 using LeafCrunch.Utilities;
 using LeafCrunch.Utilities.Entities;
+using LeafCrunch.Utilities.Sound;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace LeafCrunch.GameObjects.Items.InstantItems
 {
@@ -21,8 +23,8 @@ namespace LeafCrunch.GameObjects.Items.InstantItems
         override public Image CurrentImage { get { return _image; } }
 
         private System.Media.SoundPlayer _soundPlayer = new System.Media.SoundPlayer();
-        
-        public System.Media.SoundPlayer SoundPlayer
+
+        /*public System.Media.SoundPlayer SoundPlayer
         {
             get
             {
@@ -34,9 +36,27 @@ namespace LeafCrunch.GameObjects.Items.InstantItems
                 _soundPlayer.SoundLocation = SoundFile;
                 return _soundPlayer;
             }
+        }*/
+
+
+         void Play(string audioPath)
+         {
+             MediaPlayer myPlayer = new MediaPlayer();
+             var sysuri = new System.Uri(audioPath);
+             myPlayer.Open(sysuri);
+             myPlayer.Play();
+         }
+
+
+          public string SoundFile { get; set; }
+
+        private SoundController _soundController;
+
+        public SoundController SoundController
+        {
+            get { return _soundController; }
+            set { _soundController = value; }
         }
-        
-        public string SoundFile { get; set; }
 
         //create from item data loaded from file
         public Leaf (ItemData itemData) :base()
@@ -57,8 +77,11 @@ namespace LeafCrunch.GameObjects.Items.InstantItems
 
             if (!string.IsNullOrEmpty(itemData.SoundFile))
             {
-                SoundFile = itemData.SoundFile;
+               // SoundFile = itemData.SoundFile;
+                SoundController = new SoundController();
+                SoundController.SoundURI = new System.Uri(Application.StartupPath + itemData.SoundFile);
             }
+
             //only consider it initialized if we came this way for now
             IsInitialized = true;
         }
@@ -71,10 +94,15 @@ namespace LeafCrunch.GameObjects.Items.InstantItems
             if (victim != null)
             {
                 victim.ApplyItem(paramList);
-                if (SoundPlayer != null)
-                {
-                    _soundPlayer.Play();
-                }
+                //if (SoundPlayer != null)
+                //{
+                //       _soundPlayer.Play();
+                //}
+//                                if (SoundFile != null)
+  //                            { 
+    //                            Play(Application.StartupPath + "\\Sounds\\PointItems\\crunch.wav");
+      //                    }
+                if (SoundController != null) SoundController.Play(false);
             }
             return new Result() { Value = true };
         }
